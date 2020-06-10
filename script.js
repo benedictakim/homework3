6361010f0449ed9a4ac2e433005bc2a3d98d6c19
Benn <61118590+benedictakim@users.noreply.github.com>
2020-06-09 22:49:05 -0400

    modified:   script.js added pseudocode

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+//==============start pseudocode outline=======================================================
+
+//prompts for criteria
+//1. how long (8-128)
+//if from 8-128, else prompt with error message "enter a number from 8-128 inclusive"
+//2. upper, lower, numbers, symbols
+//if none chosen, "error message: chose at least one criteria"
+
+
+//variables for password character options: for UPPER, lower, numbers, symbols
+
+
+//function to generate password with the criteria
+
+
+//=========end pseudocode outline==========================================================
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
