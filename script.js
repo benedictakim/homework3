6e39957383e8131461f02132f9ec682331566fd0
Benn <61118590+benedictakim@users.noreply.github.com>
2020-06-22 19:20:16 -0400

    modified:   index.html -reverted to original html modified:   script.js -fixed onlick

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-document.querySelector("#generate").addEventListener("click", writePassword);
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -9,6 +10,8 @@ function writePassword() {
 
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 //arrays
 var upper = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -23,8 +26,9 @@ var lowerinput;
 var numbersinput;
 var symbolsinput;
 
-//1. how long (8-128)
+
 function generatePassword() {
+//1. how long (8-128)
   var lengthinput = (prompt("How many chracters would you like in your password? (8-128 inclusive)"));
 
   //while loop for checking 8-128, error if outside 8-128 or null; confirm input
@@ -42,14 +46,32 @@ function generatePassword() {
   var numbersinput = confirm("numbers? Click OK for Yes; Click Cancel for NO");   
   var symbolsinput = confirm("symbols? Click OK for Yes; Click Cancel for NO");
 
-  // Loop if answer is outside the parameters 
+  // Loop if none of the options are chosen
   while(upperinput === false && lowerinput === false && numbersinput === false && symbolsinput === false) {
-  alert("You must choose at least one parameter or in other words, click OK on at least one of the options");
+  alert("You must choose at least one parameter, or in other words, click OK on at least one of the options");
   var upperinput = confirm("upper case letter? Click OK for Yes; Click Cancel for NO");
   var lowerinput = confirm("lower case letters? Click OK for Yes; Click Cancel for NO");
   var numbersinput = confirm("numbers? Click OK for Yes; Click Cancel for NO");   
   var symbolsinput = confirm("symbols? Click OK for Yes; Click Cancel for NO");
+  }
 
 //3. Stringing password characters
+  var newpassword = [];
+  if (upperinput) {
+    newpassword.concat(upper);
+  }
+  if (lowerinput) {
+    newpassword.concat(lower);
+  }
+  if (numbersinput) {
+    newpassword.concat(number);
+  }
+  if (symbolsinput) {
+    newpassword.concat(symbol);
+  }
 
+  for (var i = 0; i <= lengthinput; i++) {
+    var newpassword = newpassword[Math.floor(Math.random() * newpassword.length)];
+    return(newpassword);      
+  }
 }
